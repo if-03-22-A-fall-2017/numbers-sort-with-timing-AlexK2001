4af8a850ad19053844bf212648c883e92c9cbfac
AlexK2001 <31956992+AlexK2001@users.noreply.github.com>
2018-02-14 18:35:37 +0100

    Add files via upload

---
+++ numbers-sort-with-timing-AlexK2001-kirchmair-alexander/sorting_algorithms.h
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Klasse: <your class name here>
+ * ---------------------------------------------------------
+ * Exercise Number: 0
+ * File:			sorting_algorithms.h
+ * Author:			Peter Bauer
+ * Due Date:		#due#
+ * ----------------------------------------------------------
+ * Description:
+ * <Add description>
+ * ----------------------------------------------------------
+ */
+#ifndef ___SORTING_ALGORITHMS_H
+#define ___SORTING_ALGORITHMS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void bubble_sort(int* array, int small);
+void insertion_sort(int* array, int small);
+int* init_random(int* array, int small);
+
+/**
+*** @file sorting_algorithms.h
+*** @brief Description of sorting algorithms
+*** @page Sorting Algorithms
+*** Basically two functions to sort arrays of integer elements. Furthermore, a function
+*** to initialize an array with a number of random elements.
+*/
+
+
+#endif
